f8e0d8edb7da696014cadfa87c21d9b83ed63fc3
bae121 <akpablielikembright999@gmail.com>
2025-09-19 18:30:26 +0100

    patterrn_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,13 @@
+# User's sizing fo square pattern
+size = int(input("Enter the size of the pattern: "))
+
+# Row counter
+row = 0
+
+# Outer while loop for rows
+while row < size:
+    # Inner for loop for columns (printing asterisks in one row)
+    for col in range(size):
+        print("*", end="")  # print star without moving to a new line
+    print()  # move to the next line after finishing a row
+    row += 1
