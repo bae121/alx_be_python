244332964e33ca5360312d93283807d3b70603dc
bae121 <akpablielikembright999@gmail.com>
2025-09-20 20:03:15 +0100

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,7 +7,7 @@ match priority:
         if  time_bound == 'yes':
             print(f'Reminder: {task} is a high priority task that requires immediate attention today!')
         elif time_bound == 'no':
-            print(f'note: {task} is a high priority task. Consider completing it when you have free time.')
+            print(f'Note: {task} is a high priority task. Consider completing it when you have free time.')
     case 'medium':
         if  time_bound == 'yes':
             print(f'Reminder: {task} is a medium priority task that requires immediate attention today!')
