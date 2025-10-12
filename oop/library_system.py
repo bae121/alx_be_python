0be1ef32bbd0281eda8ec8c1e5ea014ab06de835
bae121 <akpablielikembright999@gmail.com>
2025-10-12 19:37:22 +0100

    add library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,25 +6,25 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return "Book: " + self.title + " by " + self.author
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
-        super().__init__(title, author)
+        Book.__init__(self, title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {super().__str__()} [{self.file_size}MB]"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
-        super().__init__(title, author)
+        Book.__init__(self, title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {super().__str__()} [{self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -32,11 +32,9 @@ class Library:
         self.books = []
 
     def add_book(self, book: Book):
-        self.books.append(book)
+        self.books += [book]
 
     def list_books(self):
-        if not self.books:
-            print("The library is empty.")
-        else:
-            for book in self.books:
-                print(book)
+        for book in self.books:
+            print(book)
+
